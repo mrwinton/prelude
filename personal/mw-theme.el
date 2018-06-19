df8a65b394830ae0aeee6ed2e48eee6ddb873a42
Michael Winton <wintonmr@gmail.com>
2018-06-19 20:01:06 +0200

    Override theme when in GUI emacs

--- personal/mw-theme.el
+++ personal/mw-theme.el
@@ -1,6 +1,7 @@
-(prelude-require-package 'snazzy-theme)
-(setq prelude-theme 'snazzy)
-(load-theme 'snazzy)
+(when (display-graphic-p)
+  (prelude-require-package 'snazzy-theme)
+  (setq prelude-theme 'snazzy)
+  (load-theme 'snazzy))
 
 (setq-default initial-scratch-message
               (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))

5e074e9e43a08d86ef3151bceb244036ef6977bb
Michael Winton <wintonmr@gmail.com>
2018-06-19 18:12:28 +0200

    Add purcell's initial scratch message

--- personal/mw-theme.el
+++ personal/mw-theme.el
@@ -1,3 +1,6 @@
 (prelude-require-package 'snazzy-theme)
 (setq prelude-theme 'snazzy)
 (load-theme 'snazzy)
+
+(setq-default initial-scratch-message
+              (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))

9d171d4cdba15b1c7f3db431e80879f7e8b62f0f
Michael Winton <wintonmr@gmail.com>
2018-07-17 13:48:21 +0200

    Add org-mode preferences

--- personal/mw-org.el
+++ personal/mw-org.el
@@ -1,3 +1,62 @@
 ;;----------------------------------------------------------------------------
 ;; org-mode
 ;;----------------------------------------------------------------------------
+
+(setq org-directory "~/org")
+
+(defun org-file-path (filename)
+  "Return the absolute address of an org file, given its relative name."
+  (concat (file-name-as-directory org-directory) filename))
+
+(setq org-inbox-file "~/org/inbox.org")
+(setq org-index-file (org-file-path "index.org"))
+(setq org-archive-location
+      (concat (org-file-path "archive.org") "::* From %s"))
+
+;; quick templates
+(setq org-capture-templates
+      '(("b" "Blog idea"
+         entry
+         (file (org-file-path "blog-ideas.org"))
+         "* %?\n")
+
+        ("e" "Email" entry
+         (file+headline org-index-file "Inbox")
+         "* TODO %?\n\n%a\n\n")
+
+        ("f" "Finished book"
+         table-line (file "~/documents/notes/books-read.org")
+         "| %^{Title} | %^{Author} | %u |")
+
+        ("r" "Reading"
+         checkitem
+         (file (org-file-path "to-read.org")))
+
+        ("t" "Todo"
+         entry
+         (file+headline org-index-file "Inbox")
+         "* TODO %?\n")))
+
+
+;; Handy keybindings
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-cc" 'org-capture)
+
+;; Open the master org TODO list with "C-c i"
+(defun mw/open-index-file ()
+  "Open the master org TODO list."
+  (interactive)
+  (hrs/copy-tasks-from-inbox)
+  (find-file org-index-file)
+  (flycheck-mode -1)
+  (end-of-buffer))
+
+(global-set-key (kbd "C-c i") 'hrs/open-index-file)
+
+;; Open a new TODO with "M-n"
+(defun org-capture-todo ()
+  (interactive)
+  (org-capture :keys "t"))
+
+(global-set-key (kbd "M-n") 'org-capture-todo)
